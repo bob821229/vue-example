806d5a65d6ab5448b1ce572baabcc6f42e8f3ab4
bob821229 <blue821229@gmail.com>
2024-07-19 10:01:52 +0800

    人員資料表更新

--- src/views/PersonnelInformationForm.vue
+++ src/views/PersonnelInformationForm.vue
@@ -1,6 +1,6 @@
 <template>
 
-
+    <!-- <n-button @click="test">Default</n-button> -->
 
     <!-- 模板内容 -->
     <n-space vertical class="n-form-wrap">
@@ -9,22 +9,46 @@
                 <n-gi :span="24">
                     <n-divider>基本資料</n-divider>
                 </n-gi>
-                <n-form-item-gi :span="8" label="人員編號 *">
+                <n-form-item-gi :span="8">
+                    <template #label>
+                        人員編號
+                        <span style="color: red">*</span>
+                    </template>
                     <n-input v-model:value="model.employeeId" placeholder="人員編號" />
                 </n-form-item-gi>
-                <n-form-item-gi :span="8" label="進用部門 *">
+                <n-form-item-gi :span="8">
+                    <template #label>
+                        進用部門
+                        <span style="color: red">*</span>
+                    </template>
                     <n-select v-model:value="model.department" placeholder="進用部門" :options="departmentOptions" />
                 </n-form-item-gi>
-                <n-form-item-gi :span="8" label="到職日 *">
+                <n-form-item-gi :span="8">
+                    <template #label>
+                        到職日
+                        <span style="color: red">*</span>
+                    </template>
                     <n-date-picker v-model:value="model.arrivalDate" type="date" />
                 </n-form-item-gi>
-                <n-form-item-gi :span="8" label="姓名 *">
+                <n-form-item-gi :span="8">
+                    <template #label>
+                        姓名
+                        <span style="color: red">*</span>
+                    </template>
                     <n-input v-model:value="model.name" placeholder="姓名" />
                 </n-form-item-gi>
-                <n-form-item-gi :span="12" label="身分證號 *">
+                <n-form-item-gi :span="8">
+                    <template #label>
+                        身分證號
+                        <span style="color: red">*</span>
+                    </template>
                     <n-input v-model:value="model.idNumber" placeholder="身分證號" show-count :maxlength="10" />
                 </n-form-item-gi>
-                <n-form-item-gi :span="4" label="性別 *">
+                <n-form-item-gi :span="5">
+                    <template #label>
+                        性別
+                        <span style="color: red">*</span>
+                    </template>
                     <n-radio-group v-model:value="model.sex" name="sex">
                         <n-space>
                             <n-radio value="男性">
@@ -36,21 +60,42 @@
                         </n-space>
                     </n-radio-group>
                 </n-form-item-gi>
+                <n-form-item-gi :span="3" label="大頭照">
+                    <n-upload action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f"
+                        :default-file-list="previewFileList" list-type="image-card" max=1 @preview="handlePreview" />
+                    <n-modal v-model:show="showModal" preset="card" style="width: 600px" title="一张很酷的图片">
+                        <img :src="previewImageUrl" style="width: 100%">
+                    </n-modal>
+                </n-form-item-gi>
                 <n-form-item-gi :span="12" label="連絡電話">
-                    <n-input v-model:value="model.homePhone" placeholder="連絡電話" />
+                    <n-input v-model:value="model.homePhone" placeholder="連絡電話" :allow-input="onlyAllowNumber" />
                 </n-form-item-gi>
                 <n-form-item-gi :span="12" label="行動電話">
-                    <n-input v-model:value="model.phone" placeholder="行動電話" show-count :maxlength="10" />
+                    <n-input v-model:value="model.phone" placeholder="行動電話" show-count :maxlength="10"
+                        :allow-input="onlyAllowNumber" />
                 </n-form-item-gi>
-                <n-form-item-gi :span="24" label="戶籍地址 *">
+                <n-form-item-gi :span="24">
+                    <template #label>
+                        戶籍地址
+                        <span style="color: red">*</span>
+                    </template>
                     <n-input v-model:value="model.residenceAddress" placeholder="戶籍地址" />
                 </n-form-item-gi>
                 <n-form-item-gi :span="24" label="通訊地址 *">
+                    <template #label>
+                        通訊地址
+                        <span style="color: red">*</span>
+                    </template>
                     <n-input v-model:value="model.mailingAddress" placeholder="通訊地址" />
-                    <n-button type="tertiary" @click="copyResidenceAddress()">與戶籍地址相同</n-button>
+                    <n-button style="margin-left: 5px;" type="tertiary"
+                        @click="copyResidenceAddress()">與戶籍地址相同</n-button>
                 </n-form-item-gi>
-                <n-form-item-gi :span="24" label="E-mail *">
-                    <n-input v-model:value="model.phone" placeholder="E-mail" />
+                <n-form-item-gi :span="24">
+                    <template #label>
+                        E-mail
+                        <span style="color: red">*</span>
+                    </template>
+                    <n-input v-model:value="model.email" placeholder="E-mail" />
                 </n-form-item-gi>
                 <n-form-item-gi :span="8" label="特殊身分">
                     <n-checkbox-group v-model:value="model.specialStatus">
@@ -76,7 +121,11 @@
                         </n-space>
                     </n-checkbox-group>
                 </n-form-item-gi>
-                <n-form-item-gi :span="8" label="婚姻狀況 *">
+                <n-form-item-gi :span="8">
+                    <template #label>
+                        婚姻狀況
+                        <span style="color: red">*</span>
+                    </template>
                     <n-radio-group v-model:value="model.maritalStatus" name="radiogroup1">
                         <n-space>
                             <n-radio value="已婚">
@@ -130,7 +179,8 @@
                         <n-input v-model:value="model.workExperience[index].position" placeholder="職稱" />
                     </n-form-item-gi>
                     <n-form-item-gi :span="3" label="薪資(元)">
-                        <n-input v-model:value="model.workExperience[index].salary" placeholder="薪資">
+                        <n-input v-model:value="model.workExperience[index].salary" placeholder="薪資"
+                            :allow-input="onlyAllowNumber">
                             <template #suffix>
                                 元
                             </template>
@@ -184,7 +234,7 @@
                 </n-form-item-gi>
                 <n-form-item-gi :span="10" label="已選擇語言">
                     <n-space>
-                        <n-tag v-for="(l, idx) in model.languages" :key="idx" type="info" closable
+                        <n-tag v-for="(l, idx) in model.languages" :key="idx" type="success" closable
                             @close="handleClose('language', idx)"> {{ l }}
                         </n-tag>
                     </n-space>
@@ -203,27 +253,105 @@
                             其他
                         </n-checkbox>
                         <n-input size="small" v-if="othercomputerExpertise" v-model:value="customComputerExpertise"
-                            placeholder="請輸入其他語言" @keyup.enter="addCustom('computerExpertise')" />
+                            placeholder="請輸入其他專長" @keyup.enter="addCustom('computerExpertise')" />
                     </n-space>
 
                 </n-form-item-gi>
                 <n-form-item-gi :span="10" label="已選擇電腦專長">
                     <n-space>
-                        <n-tag v-for="(l, idx) in model.computerExpertise" :key="idx" type="info" closable
+                        <n-tag v-for="(l, idx) in model.computerExpertise" :key="idx" type="success" closable
                             @close="handleClose('computerExpertise', idx)">
                             {{ l }}
                         </n-tag>
                     </n-space>
                 </n-form-item-gi>
                 <n-form-item-gi :span="24" label="專業證照">
-                    <n-input v-model:value="model.idNumber" placeholder="專業證照" />
+                    <n-input v-model:value="model.professionalLicense" placeholder="專業證照" />
                 </n-form-item-gi>
+                <n-form-item-gi :span="24" label="知識技能">
+                    <n-input v-model:value="model.knowledgeAndSkills" placeholder="Ex:設備操作、簡報技巧...請自我表述" />
+                </n-form-item-gi>
+                <n-gi :span="24">
+                    <n-divider>緊急聯絡人</n-divider>
+                </n-gi>
+                <n-form-item-gi :span="6" label="姓名">
+                    <n-input v-model:value="model.emergencyContact.name" placeholder="姓名" />
+                </n-form-item-gi>
+                <n-form-item-gi :span="6" label="關係">
+                    <n-input v-model:value="model.emergencyContact.relationship" placeholder="關係" />
+                </n-form-item-gi>
+                <n-form-item-gi :span="6" label="連絡電話">
+                    <n-input v-model:value="model.emergencyContact.phone" placeholder="連絡電話"
+                        :allow-input="onlyAllowNumber" />
+                </n-form-item-gi>
+                <n-form-item-gi :span="6" label="行動電話">
+                    <n-input v-model:value="model.emergencyContact.mobile" placeholder="行動電話" show-count :maxlength="10"
+                        :allow-input="onlyAllowNumber" />
+                </n-form-item-gi>
+                <n-gi :span="24">
+                    <n-divider>國泰世華銀行 薪資撥入帳號 <span style="color:red;margin-left: 10px;">(請詳細填寫)</span></n-divider>
+                </n-gi>
+                <n-form-item-gi :span="12">
+                    <template #label>
+                        分行名稱
+                        <span style="color: red">*</span>
+                    </template>
+                    <n-input v-model:value="model.bank.branchName" placeholder="分行名稱" />
+                </n-form-item-gi>
+                <n-form-item-gi :span="12">
+                    <template #label>
+                        帳號
+                        <span style="color: red">*</span>
+                    </template>
+                    <n-input v-model:value="model.bank.account" type="text" :allow-input="onlyAllowNumber"
+                        placeholder="帳號" show-count :maxlength="12" />
+                </n-form-item-gi>
+                <n-gi :span="24">
+                    <n-divider>身分證</n-divider>
+                </n-gi>
+                <n-form-item-gi :span="12">
+                    <n-upload multiple directory-dnd action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f" :max="5">
+                        <n-upload-dragger>
+                            <div style="margin-bottom: 12px">
+                                <n-icon size="48" :depth="3">
+                                    <archive-icon />
+                                </n-icon>
+                            </div>
+                            <n-text style="font-size: 16px">
+                                身分證照片
+                            </n-text>
+                            <n-p depth="3" style="margin: 8px 0 0 0">
+                                (正面)
+                            </n-p>
+                        </n-upload-dragger>
+                    </n-upload>
+                </n-form-item-gi>
+
+                <n-form-item-gi :span="12">
+                    <n-upload multiple directory-dnd action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f" :max="5">
+                        <n-upload-dragger>
+                            <div style="margin-bottom: 12px">
+                                <n-icon size="48" :depth="3">
+                                    <archive-icon />
+                                </n-icon>
+                            </div>
+                            <n-text style="font-size: 16px">
+                                身分證照片
+                            </n-text>
+                            <n-p depth="3" style="margin: 8px 0 0 0">
+                                (反面)
+                            </n-p>
+                        </n-upload-dragger>
+                    </n-upload>
+                </n-form-item-gi>
+
             </n-grid>
         </n-form>
 
         <p>
             {{ model }}
         </p>
+
     </n-space>
 
 </template>
@@ -232,15 +360,21 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { FormInst, FormItemRule, useMessage } from 'naive-ui'
+import { UploadFileInfo } from 'naive-ui'
 import dayjs from 'dayjs'
 
 import {
     Add as AddIcon,
     Remove as CloseCircleIcon,
     School,
+    ArchiveOutline as ArchiveIcon
 
 } from '@vicons/ionicons5'
-import { number } from 'echarts'
+
+function test() {
+    model.value = { "employeeId": "A2354", "department": "研究一所", "arrivalDate": 1719417600000, "name": "王大明", "idNumber": "F123456789", "sex": "男性", "birthday": null, "homePhone": "033124569", "phone": "0912345648", "residenceAddress": "新北市淡水區中正東路38號", "mailingAddress": "新北市淡水區中正東路38號", "email": "abc@gmail.com", "specialStatus": ["原住民", "身心障礙"], "drvingLicense": ["汽車"], "maritalStatus": "已婚", "schools": [{ "name": "台大", "department": "數學", "period": [1717171200000, 1717171200000] }], "workExperience": [{ "company": "台農院", "position": "前端工程師", "salary": "22000", "leavingReason": "錢太少", "period": [1711900800000, 1717171200000] }], "languages": ["國語", "台語", "泰語"], "computerExpertise": ["Word", "Excel"], "professionalLicense": "化學丙級", "knowledgeAndSkills": "簡報技巧", "emergencyContact": { "name": "林小美", "relationship": "母子", "phone": "0332546985", "mobile": "0955568112" }, "bank": { "account": "443515454848", "branchName": "南崁" } }
+}
+//語言選項
 const languagesOptions = ref([
     {
         value: '國語',
@@ -263,6 +397,7 @@ const languagesOptions = ref([
         checked: false
     }
 ])
+//電腦專長選項
 const computerExpertiseOptions = ref([
     {
         value: 'Word',
@@ -285,14 +420,8 @@ const computerExpertiseOptions = ref([
         checked: false
     }
 ])
-const otherLanguage = ref(false)
-const othercomputerExpertise = ref(false)
-const value = ref([
-    {
-        key: '',
-        value: ''
-    }
-])
+const otherLanguage = ref(false)//其他語言
+const othercomputerExpertise = ref(false)//其他電腦專長
 // 個人資訊
 const model = ref({
     employeeId: '',//人員編號
@@ -328,7 +457,18 @@ const model = ref({
     ],//職務經歷
     languages: [] as string[],//語言能力
     computerExpertise: [] as string[],//電腦專長
-    professionalLicense: ''
+    professionalLicense: '',//專業證照
+    knowledgeAndSkills: '',//知識技能
+    emergencyContact: {
+        name: '', // 緊急聯絡人姓名
+        relationship: '', // 關係
+        phone: '', // 連絡電話
+        mobile: '' // 行動電話
+    },//緊急聯絡人資訊
+    bank: {
+        account: '',//銀行帳號
+        branchName: ''//分行名稱
+    },//銀行資訊
 
 })
 //檢查條件
@@ -409,15 +549,15 @@ function deleteWorkExperience(idx: number) {
     if (model.value.workExperience.length > 1) {
         model.value.workExperience.splice(idx, 1)
     } else {
-        alert("至少要一個教育經歷")
+        alert("至少要一個職務經歷")
     }
 }
 // 複製 戶籍地址資料=>通訊地址資料
 function copyResidenceAddress() {
     model.value.mailingAddress = model.value.residenceAddress;
 }
-const customLanguage = ref('')
-const customComputerExpertise = ref('')
+const customLanguage = ref('')//其他語言
+const customComputerExpertise = ref('')//其他電腦專長
 //增加語言
 function addCustom(type: string) {
     if (type == 'language') {
@@ -442,6 +582,20 @@ function handleClose(type: string, idx: number) {
     }
 }
 
+const showModal = ref(false)
+const previewImageUrl = ref('')
+const previewFileList = ref<UploadFileInfo[]>([
+
+])
+
+function handlePreview(file: UploadFileInfo) {
+    const { url } = file
+    previewImageUrl.value = url as string
+    showModal.value = true
+}
+
+//只能輸入數字
+const onlyAllowNumber = (value: string) => !value || /^\d+$/.test(value);
 </script>
 
 
--- src/views/PersonnelInformationForm2.vue
+++ src/views/PersonnelInformationForm2.vue
@@ -1,67 +1,4 @@
 <template>
-    <div class="content">
-        <div class="row">
-            <div class="col-md-4">
-                <label for="inputEmail4" class="form-label">人員編號</label>
-                <input type="email" class="form-control" id="inputEmail4">
-            </div>
-            <div class="col-md-4">
-                <label for="inputState" class="form-label">進用部門</label>
-                <select id="inputState" class="form-select">
-                    <option disabled value="">請選擇部門</option>
-                    <option value="數發處">數發處</option>
-                    <option value="研究一所">研究一所</option>
-                    <option value="研究二所">研究二所</option>
-                    <option value="研究三所">研究三所</option>
-                    <option value="研究四所">研究四所</option>
-                </select>
-            </div>
-            <div class="col-md-4">
-                <label for="inputState" class="form-label">進用部門</label>
-
-            </div>
-            <div class="col-12">
-                <label for="inputAddress" class="form-label">Address</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-            </div>
-            <div class="col-12">
-                <label for="inputAddress2" class="form-label">Address 2</label>
-                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-            </div>
-            <div class="col-md-6">
-                <label for="inputCity" class="form-label">City</label>
-                <input type="text" class="form-control" id="inputCity">
-            </div>
-            <div class="col-md-4">
-                <label for="inputState" class="form-label">進用部門</label>
-                <select id="inputState" class="form-select">
-                    <option disabled value="">請選擇部門</option>
-                    <option value="數發處">數發處</option>
-                    <option value="研究一所">研究一所</option>
-                    <option value="研究二所">研究二所</option>
-                    <option value="研究三所">研究三所</option>
-                    <option value="研究四所">研究四所</option>
-                </select>
-            </div>
-            <div class="col-md-2">
-                <label for="inputZip" class="form-label">Zip</label>
-                <input type="text" class="form-control" id="inputZip">
-            </div>
-            <div class="col-12">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="gridCheck">
-                    <label class="form-check-label" for="gridCheck">
-                        Check me out
-                    </label>
-                </div>
-            </div>
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary">Sign in</button>
-            </div>
-
-        </div>
-        <input type="month">
-    </div>
 
 </template>
 
@@ -70,13 +7,4 @@ import { ref } from 'vue';
 
 </script>
 
-<style scoped lang="scss">
-.content {
-    width: 80%;
-    margin: 100px auto;
-    padding: 10px;
-    border: 2px solid #ccc;
-    border-radius: 10px;
-    background-color: #f9f9f9
-}
-</style>
+<style scoped lang="scss"></style>
